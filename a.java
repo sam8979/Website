4c0a043be5fbc71f835718544b2bcffe10308ae5
Samuel <101493296+sam8979@users.noreply.github.com>
2023-03-01 23:38:15 +0530

    Update a.java

---
+++ a.java
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+       
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First Web code</title>
+<style>
+.container {
+    background-color:  pink;
+    border:2px solid blue;
+    margin: 10px;
+    padding: 40px;
+
+}
+.love{
+    background-color: green;
+}
+.newtext{
+    color: red;
+}
+</style>
+
+
+
+<nav>Home</nav>
+<nav>Blog</nav>
+<nav>Category</nav>
+<nav>Education</nav>
+<nav>Feedback</nav>
+<nav>Donation$</nav>
+
+
+
+</head>
+
+<body>
+    <h1>WELCOME TO SAM'S CODING</h1>    
+    <div id="firstcontainer" class="container">
+        <button id="click" onclick="direct()" >Click me </button>
+        <p>Hi Guys this is the coding that available for free</p>
+
+
+    </div>
+    <div class="container">
+        <p>This is text will change the attributes after clicks the upper box</p>
+
+
+       
+        
+    </div>
+
+</body>
+<link rel="stylesheet" href="first.css">
+<script src="first.js"></script>
+
+</html>
